db5e1d8e200f6d29029a16b1b7cd6ce3b0867733
agesinardigital <age.ab99@gmail.com>
2024-05-15 19:29:57 +0700

    Features & How it Works done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import Highlights from "./components/Highlight";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
 
+import * as Sentry from "@sentry/react";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+
 const App = () => {
- return (
-  <main className="bg-black">
-    <Navbar />
-    <Hero />
-    <Highlights />
-    <Model />
-  </main>
- )
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+    </main>
+  );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,3 +1,20 @@
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggelActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
+
 export const animateWithGsapTimeline = (
   timeline,
   rotationRef,
